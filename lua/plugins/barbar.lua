daf9ffbe906c499d3c61875ac0756aa24c8109e5
LilleAila <olai.solsvik@gmail.com>
2023-06-26 17:02:48 +0200

    Fixed errors when starting and disabled discord

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -5,7 +5,7 @@ return {
             "nvim-tree/nvim-web-devicons", "lewis6991/gitsigns.nvim"
         },
         init = function()
-            vim.g.barbar_auto_setup = false
+            vim.g.barbar_auto_setup = true
             -- vim.g.barbar_theme = "catppuccin"
         end,
         opts = {
